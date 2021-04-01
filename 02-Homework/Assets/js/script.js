8bd1ea416d275e6adc648fa41e32efd85621d675
eamon <eamon.matheos@gmail.com>
2021-04-01 16:58:00 -0400

    shortend it and fixed it

--- 02-Homework/Assets/js/script.js
+++ 02-Homework/Assets/js/script.js
@@ -10,14 +10,11 @@ var sData = {};
 console.log(time);
 for(x of hours){
 if(x == time ){
-var present = $("#"+this)
-present.addClass("present")
+    ("#"+this).addClass("present")
 }else if(x > time){
-var future = $("#"+this)
-curennt.addClass("future")
+    ("#"+this).addClass("future")
 }else{
-    var past = $("#"+this)
-    past.addClass("past")    
+    ("#"+this).addClass("past")    
 }
 }
 
